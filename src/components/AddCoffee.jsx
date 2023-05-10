1ba3e2ff75e0833109879a904963f039de750287
Aminul667 <rahathssn@gmail.com>
2023-05-10 12:47:11 +0600

    68-5 Send Coffee data to the server and store in the mongodb database

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import Swal from "sweetalert2";
 
 const AddCoffee = () => {
-  const handleAddCoffee = event => {
+  const handleAddCoffee = (event) => {
     event.preventDefault();
 
     const form = event.target;
@@ -14,9 +14,38 @@ const AddCoffee = () => {
     const detail = form.detail.value;
     const photo = form.photo.value;
 
-    const newCoffee = {name, quantity, supplier, test, category, detail, photo};
+    const newCoffee = {
+      name,
+      quantity,
+      supplier,
+      test,
+      category,
+      detail,
+      photo,
+    };
     console.log(newCoffee);
-  }
+
+    // send data to the server
+    fetch("http://localhost:5000/coffee", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newCoffee),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.insertedId) {
+          Swal.fire({
+            title: "Success!",
+            text: "User Added Successfully",
+            icon: "success",
+            confirmButtonText: "Cool",
+          });
+        }
+      });
+  };
   return (
     <div className="bg-[#F4F3F0] p-24">
       <h2 className="text-3xl font-extrabold">Add Coffee</h2>
@@ -124,7 +153,9 @@ const AddCoffee = () => {
             </label>
           </div>
         </div>
-        <button className="btn btn-block" type="submit">Add Coffee</button>
+        <button className="btn btn-block" type="submit">
+          Add Coffee
+        </button>
       </form>
     </div>
   );
